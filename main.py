9f14a639407b26ddf149e627b3e899c7cb7f3fec
Mandy Choi <hello@mandychoi.com.au>
2025-08-27 01:18:03 +0100

    main to run git diff command

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from the-nitpicker!")
+import subprocess
 
+def git_diff():
+    command = ["git", "diff"]
+
+    try:
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        output_string = result.stdout
+        print(output_string)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error: {e.stderr}")
+
+def main():
+    git_diff()
 
 if __name__ == "__main__":
     main()
